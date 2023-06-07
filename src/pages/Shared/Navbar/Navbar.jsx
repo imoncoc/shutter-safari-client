d105be33b208b231b02322b4a5fe1bdfc22122b1
Md Imon Hossain <imoncoc0@gmail.com>
2023-06-08 01:16:40 +0600

    user img added into navbar

--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link, NavLink } from "react-router-dom";
 import { AuthContext } from "../../../providers/AuthProvider";
 import './Navbar.css'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCameraRetro, faMoon, faSun } from "@fortawesome/free-solid-svg-icons";
+import { faCameraRetro, faMoon, faSun, faUser } from "@fortawesome/free-solid-svg-icons";
 import ThirdButton from "../../../components/Reuseable/Button/ThirdButton/ThirdButton";
 import PrimaryButton from "../../../components/Reuseable/Button/PrimaryButton/PrimaryButton";
 
@@ -100,18 +100,74 @@ const Navbar = () => {
               </li>
               <li className="me-2">
                 {user ? (
-                  // <button onClick={handleLogOut} className="btn btn-danger">
-                  //   {" "}
-                  //   Logout
-                  // </button>
-                  <span onClick={handleLogOut}>
-                    <ThirdButton name={"Logout"}></ThirdButton>
-                  </span>
+                  <div>
+                    <img title={user?.displayName} className="img-fluid user-img me-2" src={user.photoURL} alt="" />
+                    <span onClick={handleLogOut}>
+                      <ThirdButton name={"Logout"}></ThirdButton>
+                    </span>
+                  </div>
                 ) : (
                   <Link to="/login">
                     <PrimaryButton name={"Login"}></PrimaryButton>
                   </Link>
                 )}
+
+                {/* {user ? (
+                  <div
+                    className="d-flex justify-content-around align-items-center text-center"
+                    style={{ width: "20rem" }}
+                  >
+                    <div className="d-flex align-items-center">
+                      {user.photoURL ? (
+                        <img
+                          src={user?.photoURL}
+                          title={
+                            user.displayName
+                              ? user.displayName
+                              : "No Name Found!"
+                          }
+                          alt=""
+                          className="user-img img-fluid"
+                        />
+                      ) : (
+                        <div
+                          className="phone-icon"
+                          title={
+                            user?.displayName
+                              ? user.displayName
+                              : "No Name Found!"
+                          }
+                        >
+                          <FontAwesomeIcon
+                            className="user-icon"
+                            icon={faUser}
+                          />
+                        </div>
+                      )}
+                      <p className="user-email my-2 ms-1 fw-semibold">
+                        {user.email ? (
+                          // <span>{user.email.substring(0, 20)}</span>
+                          <span>
+                            {user.email.substring(0, 3)}....
+                            {user.email.substring(user.email.length - 10)}
+                          </span>
+                        ) : (
+                          <span style={{ fontSize: "0.8rem" }}>Mr/Mrs.</span>
+                        )}
+                      </p>
+                    </div>
+                    <button
+                      className="btn btn-outline-danger"
+                      onClick={handleLogOut}
+                    >
+                      Logout
+                    </button>
+                  </div>
+                ) : (
+                  <Link to="/login">
+                    <button className="btn btn-outline-info">Login</button>
+                  </Link>
+                )} */}
               </li>
               <li className="my-1">
                 <div onClick={handleToggle}>
